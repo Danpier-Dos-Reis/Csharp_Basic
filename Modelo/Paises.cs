297025ccb31d75fdabfe8db22a7408de74627206
Danpier Dos Reis <danpier_dr@hotmail.com>
2023-01-01 17:56:08 -0300

    I made a list of countries and a I printed all values of one country with two for loops

---
+++ Modelo/Paises.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Listas.Modelo
+{
+    public class Pais
+    {
+        public string? Nombre {get; set;}
+        public string? Presidente {get; set;}
+        public string? Gentilisio {get; set;}
+        public string? Religion {get; set;}
+        public int GastoPublico {get; set;}
+        public int AnnioFundacion {get; set;}
+    }
+}
