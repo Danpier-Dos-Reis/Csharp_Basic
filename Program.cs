28f23ea4dfedb6e52fe16c2dbca5bb6f5bb0f0db
Danpier Dos Reis <d6nt3_d3m0n@outlook.com>
2023-04-07 12:10:30 -0400

    Get All Data about StarWars Person

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CsharpApis;
+
+var engine = new Engine();
+string content = await engine.GetResponse();
+
+Console.WriteLine(content);
