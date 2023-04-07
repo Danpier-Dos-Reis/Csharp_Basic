c9745fc4115390270b5ddf9d83b8ff2f84034acf
Danpier Dos Reis <d6nt3_d3m0n@outlook.com>
2023-04-07 12:22:54 -0400

    Deserialize JSON

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CsharpApis;
+using QuickType;
+using Newtonsoft.Json;
 
 var engine = new Engine();
 string content = await engine.GetResponse();
 
-Console.WriteLine(content);
+SwPeople? person = new SwPeople();
+
+person = JsonConvert.DeserializeObject<SwPeople>(content);
+
+Console.WriteLine(person?.Name); //Luke Skywalker
+foreach(var prop in person.Films){
+    Console.WriteLine(prop);
+}
+
+////Print Name's Properties from Object
+// foreach(var prop in person.GetType().GetProperties()){
+//     Console.WriteLine(prop.Name);
+// }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CsharpApis
-My Knowledges in Apis
+Tools:
+* **Get JSON**
+    * From: [**Swapi**](https://swapi.py4e.com "swapi.py4e.com").
+    * Using _HttpClient_ class and _Asynchronism_ 🔂.
+* **Deserialization**
+    * VSCode Plugin: **Paste JSON as Code** by _quicktype_ team.
+    * Match C# Class with JSON using Nuget Package **Newtonsoft.Json**.
+    * Print Object Properties in Console.
+* Note:
+    * _**Deserialize is convert a Json object to our personal C# (or any programming language) object for manage these data in one project.**_
